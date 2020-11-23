56e22abecebadaece8dd164b7df713e1b62cb111
robinahl <robinahl@users.noreply.github.com>
2020-11-23 13:05:30 +0100

    Oppdatering
    Oppgaver og codeAlong

---
+++ TerjeTime_17.11.20/MvcClickerWithUnitTest/controller.js
@@ -0,0 +1,13 @@
+// controller
+function doClick() {
+    points += pointsPerClick;
+    smileyIndex = 1 - smileyIndex;
+    updateView();
+}
+
+function buyUpgrade() {
+    if (points < 10) return;
+    points -= 10;
+    pointsPerClick++;
+    updateView();
+}
